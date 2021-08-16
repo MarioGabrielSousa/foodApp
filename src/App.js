79e4d1935e71239c552bc6f3613de2250cb6d3cf
Mario Sousa <vegetalizado@gmail.com>
2021-08-16 16:24:10 +0100

    Trying to pass first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
   const APP_ID = "c59549e4";
   const APP_KEY = "22dcb7cadf1342d391729879df131f90";
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-const[counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-useEffect(() => {
- console.log('Effect has been run');
-});
+  useEffect(() => {
+    console.log("Effect has been run");
+  });
 
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button onClick={() => setCounter(counter + 1)} className="search-button" type="submit">
+        <button
+          onClick={() => setCounter(counter + 1)}
+          className="search-button"
+          type="submit"
+        >
           Search
         </button>
       </form>
